50340f032ee9058823bd200ca79c4a0a08d3162e
Gilad Elgavish <gelgavish@gmail.com>
2019-05-19 21:26:35 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/ido_gilad/MainActivity.java
+++ app/src/main/java/com/example/ido_gilad/MainActivity.java
@@ -98,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
                 check("/");
             }
         });
-        ,kwrvflware gflaweofgrao;i/fhaerio;gh er;okglierohgjer p gkbaerli gjkr glikewrj.g er;jkg haer;ou gaerg;ouaerg oaeu rgougsrh; gurh;ohg;osrehgo;hitr s;oh;strh; oisrt hoijs;oh ;orthor thr hsrihosrth osrth rth;sflgh;sit h;otshlkgh sfh;oit;h oistr
+        
         btn[17].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
